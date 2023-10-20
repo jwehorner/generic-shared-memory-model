45b01810699cdbeadd241ec4bae70e1f9752f308
James Horner <James.Horner@nrc-cnrc.gc.ca>
2023-10-20 10:19:42 -0400

    (Feat) Added flag for print warnings, fixed discarded return value warning from ftruncate on UNIX, fixed comment formatting.

--- GenericSharedMemoryModel.hpp
+++ GenericSharedMemoryModel.hpp
@@ -13,7 +13,7 @@
 #ifndef GENERIC_SHARED_MEMORY_MODEL_H
 #define GENERIC_SHARED_MEMORY_MODEL_H
 
-// System Libraries
+// C++ Standard Library Headers
 #include <cstring>
 #include <mutex>
 #include <string>
@@ -24,9 +24,9 @@
 #include <Windows.h>
 #include <tchar.h>
 #else
-#include <fcntl.h>      //Needed for read/write definitions
-#include <unistd.h>     //Needed for close()
-#include <sys/mman.h>   //For POSIX shared memory via shm_open
+#include <fcntl.h>      // Needed for read/write definitions
+#include <unistd.h>     // Needed for close()
+#include <sys/mman.h>   // For POSIX shared memory via shm_open
 #endif
 
 /**
@@ -41,11 +41,19 @@
 template<typename T>
 class GenericSharedMemoryModel {
 public:
-    ///Constructor for the GenericSharedMemoryModel class that initialises members, but does not connect shared memory.
-    GenericSharedMemoryModel(const std::string name) : m_name(name) {m_is_connected = false;};
+    /// Constructor for the GenericSharedMemoryModel class that initialises members, but does not connect shared memory.
+    GenericSharedMemoryModel(const std::string name, const bool log_warnings = false) : 
+        m_name(name),
+        m_log_warnings(log_warnings)
+    {
+        m_is_connected = false;
+    }
 	
-    ///Destructor for the GenericSharedMemoryModel class that disconnects from shared memory if the object is deleted.
-    ~GenericSharedMemoryModel() {disconnect();};
+    /// Destructor for the GenericSharedMemoryModel class that disconnects from shared memory if the object is deleted.
+    ~GenericSharedMemoryModel() 
+    {
+        disconnect();
+    }
 
     /**
      * @brief Function connect() is used to connect the GenericSharedMemoryModel object to the shared memory segment.
@@ -90,25 +98,25 @@ public:
 		memcpy(data, &new_data, sizeof(T));
 	}
 
-    ///Public member for the structure that is mapped to the shared memory segment upon the calling of connect().
+    /// Public member for the structure that is mapped to the shared memory segment upon the calling of connect().
     T* data;
 
 private:
-    ///Private member for the status of the connection to shared memory.
+    /// Private member for the status of the connection to shared memory.
     bool m_is_connected;
-    /**
-     * @brief Name of the shared memory segment.
-     */
+    /// Flag for if warnings should be logged to the console (instead of just flagged in return values).
+    bool m_log_warnings;
+    /// Name of the shared memory segment.
     std::string m_name;
 	/// Mutex lock to protect the members of the class when accessing concurrently.
 	std::mutex m_member_lock;
 
 #ifdef WIN32
-    ///Handle to file mapping object for the connection to shared memory in Windows.
-    HANDLE hMapFile;
+    /// Handle to file mapping object for the connection to shared memory in Windows.
+    HANDLE m_file_mapping_handle;
 #else
-    //ID of the shared memory segment in a UNIX environment.
-    int hMapFile;
+    // ID of the shared memory segment in a UNIX environment.
+    int m_file_mapping_handle;
 #endif
 
 };
@@ -119,14 +127,14 @@ bool GenericSharedMemoryModel<T>::connect()
 	// Gain access to the member mutex.
 	std::scoped_lock<std::mutex> member_guard(m_member_lock);
 
-    //If shared memory is not connected already,
+    // If shared memory is not connected already,
     if(!m_is_connected) {
 #ifdef _WIN32
-        //Store the value of the handle returned by CreateFileMappingA so it can be error-checked.
+        // Store the value of the handle returned by CreateFileMappingA so it can be error-checked.
 		// 2023-02-14 JH:	Force use of CreateFileMappingA as on some platforms CreateFileMapping 
 		//					expands to CreateFileMappingW requiring a wide string which a std::string
 		// 					is not always compatible with without conversion.
-        hMapFile = CreateFileMappingA(                               
+        m_file_mapping_handle = CreateFileMappingA(                               
 			INVALID_HANDLE_VALUE,		// Create new file mapping object.
 			NULL,	                    // default security
 			PAGE_READWRITE,		        // read/write access
@@ -134,65 +142,83 @@ bool GenericSharedMemoryModel<T>::connect()
 			sizeof(T),		// maximum object size (low-order DWORD)
 			m_name.c_str());				// name of mapping object 
         
-        //If the handle is invalid,
-        if (hMapFile == NULL){
-            //Print an error message and return failure.
-            printf("Couldn't connect to shared memory with name: %s\n", m_name.c_str());
+        // If the handle is invalid,
+        if (m_file_mapping_handle == NULL){
+            // Return failure.
+            if (m_log_warnings) {
+                printf("Couldn't connect to shared memory with name: %s\n", m_name.c_str());
+            }
             m_is_connected = false;
             return false;
         }
 
-        //If the handle is valid, try to map the handle to the T structure.
-		//Cast the new file to the struct from FRL types so we can read/write easily.
+        // If the handle is valid, try to map the handle to the T structure.
+		// Cast the new file to the struct from FRL types so we can read/write easily.
         data = (T*)MapViewOfFile(
-			hMapFile,            //assign the map object to the shared data struct.
-			FILE_MAP_ALL_ACCESS, //read/write permission
+			m_file_mapping_handle,            // assign the map object to the shared data struct.
+			FILE_MAP_ALL_ACCESS, // read/write permission
 			0,
 			0,
 			sizeof(T));
 
-        //If the mapping returned an invalid memory location,
+        // If the mapping returned an invalid memory location,
         if (data == NULL){
-            //Print an error message, close the file handle, and return failure.
-            printf("Couldn't map view of file to shared memory with name: %s\n", m_name.c_str());
-            CloseHandle(hMapFile);
+            // Close the file handle, and return failure.
+            if (m_log_warnings) {
+                printf("Couldn't map view of file to shared memory with name: %s\n", m_name.c_str());
+            }
+            CloseHandle(m_file_mapping_handle);
             m_is_connected = false;
             return false;
         }        
 
-        //Set the connection state to true.
+        // Set the connection state to true.
         m_is_connected = true;
 #else
-        //Get an ID for the shared memory segment with the name.
-        hMapFile = shm_open(m_name.c_str(), O_CREAT | O_RDWR, 0666); //Create file descriptor for shared mem. Create the mem if it doesn't already exist. Set permissions to 666
+        // Get an ID for the shared memory segment with the name.
+        m_file_mapping_handle = shm_open(m_name.c_str(), O_CREAT | O_RDWR, 0666); // Create file descriptor for shared mem. Create the mem if it doesn't already exist. Set permissions to 666
 
-        //If the ID is invalid,
-        if (hMapFile < 0) {
-            //Could not create the shared memory file descriptor via shm_open()
-            //Print an error message and return failure.
-            printf("Couldn't connect to shared memory with name: %s\n", m_name.c_str());
+        // If the ID is invalid,
+        if (m_file_mapping_handle < 0) {
+            // Could not create the shared memory file descriptor via shm_open()
+            if (m_log_warnings) {
+                // Print an error message and return failure.
+                printf("Couldn't connect to shared memory with name: %s\n", m_name.c_str());
+            }
 			m_is_connected = false;
             return false;
         }
 
-        //Try to map the shared memory segment to a T structure.
-        ftruncate(hMapFile, sizeof (T));
-        data = (T *) mmap(NULL, sizeof(T), PROT_READ | PROT_WRITE, MAP_SHARED, hMapFile, 0);
+        // Try to truncate the file mapping handle to the correct size.
+        if (ftruncate(m_file_mapping_handle, sizeof (T)) != 0) {
+            // Could not truncate shared memory to the correct size.
+            if (m_log_warnings) {
+                // Print an error message and return failure.
+                printf("Couldn't truncate shared memory because of error: %d\n", errno);
+            }
+			m_is_connected = false;
+            return false;
+        }
 
-        //If the mapping is unsuccessful,
+        // Try to map the shared memory segment to a T structure.
+        data = (T *) mmap(NULL, sizeof(T), PROT_READ | PROT_WRITE, MAP_SHARED, m_file_mapping_handle, 0);
+
+        // If the mapping is unsuccessful,
         if (data == MAP_FAILED) {
-            //Print an error message, close the file handle, and return failure.
-            printf("Couldn't map view of file to shared memory with name: %s\n", m_name.c_str());
+            // Close the file handle, and return failure.
+            if (m_log_warnings) {
+                printf("Couldn't map view of file to shared memory with name: %s\n", m_name.c_str());
+            }
             m_is_connected = false;
             return false;
         }
 
-        //Set the connection state to true.
+        // Set the connection state to true.
         m_is_connected = true;
 #endif
     }
-    //If already connected or the connection process completed without returning false,
-    //Return successfully.
+    // If already connected or the connection process completed without returning false,
+    // Return successfully.
     return m_is_connected;
 }
 
@@ -202,15 +228,15 @@ bool GenericSharedMemoryModel<T>::disconnect()
 	// Gain access to the member mutex.
 	std::scoped_lock<std::mutex> member_guard(m_member_lock);
 
-    //If shared memory is connected currently,
+    // If shared memory is connected currently,
     if(m_is_connected){
 #ifdef _WIN32
         UnmapViewOfFile(data);
-        CloseHandle(hMapFile);
+        CloseHandle(m_file_mapping_handle);
         m_is_connected = false;
 #else
         munmap(data, sizeof (T));
-        close(hMapFile);
+        close(m_file_mapping_handle);
         m_is_connected = false;
 #endif
     }
